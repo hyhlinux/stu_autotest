f5c34f814a47cbd17db45016d221f80f11a532c8
tplinux <2285020853@qq.com>
2016-11-23 15:01:46 +0800

    Update test_main.py

--- test_pytest/test_main.py
+++ test_pytest/test_main.py
@@ -1,7 +1,4 @@
 # coding:utf-8 
-＃<<<<<<< HEAD
-＃=======
-＃>>>>>>> 8e268cbfe963820cd8b09d8e85534c06fce1fd99
 import pytest
 import os
 
@@ -11,16 +8,13 @@ def test_main():
 
 if __name__ == '__main__':
     # pytest.main()
-<<<<<<< HEAD
     # pytest.main("-q test_main.py")   # 指定测试文件
     os.chdir('.')
     targetpath = os.getcwd()
     print(targetpath)
     pytest.main(targetpath)             # 指定测试目录
-=======
     pytest.main("-q test_main.py")   # 指定测试文件
     # os.chdir('.')
     # targetpath = os.getcwd()
     # print(targetpath)
     # pytest.main(targetpath)             # 指定测试目录
->>>>>>> 8e268cbfe963820cd8b09d8e85534c06fce1fd99
