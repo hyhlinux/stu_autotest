2840d9e79bb13e451887fa888b2cdec4c1e53c23
tplinux <2285020853@qq.com>
2016-11-23 15:54:37 +0800

    Update test_class.py

--- test_pytest/test_class.py
+++ test_pytest/test_class.py
@@ -11,10 +11,8 @@ class TestClass(object):
 
     def test_two(self):
         x = 'hello'
-<<<<<<< HEAD
-        assert "hi" == x
-=======
+        assert "hi" != x
         assert "hello" == x
->>>>>>> 8e268cbfe963820cd8b09d8e85534c06fce1fd99
+
 
 
