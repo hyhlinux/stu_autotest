89dd514645e7bbac6e0886c36dc23748baeb3b2a
tplinux <2285020853@qq.com>
2016-11-23 15:55:22 +0800

    Update test_pytest.py

--- test_pytest/test_pytest.py
+++ test_pytest/test_pytest.py
@@ -6,8 +6,5 @@ def func(x):
 
 # 测试用例
 def test_answer():
-<<<<<<< HEAD
-    assert func(3) == 5
-=======
-    assert func(5) == 5
->>>>>>> 8e268cbfe963820cd8b09d8e85534c06fce1fd99
+    assert func(4) == 5
+    assert func(5) != 5
